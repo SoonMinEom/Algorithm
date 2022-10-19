0e9535231b2ec46d633f48f34493969246f73f68
soonmineom <skdlfma123@gmail.com>
2022-10-19 10:24:39 +0900

    stack 구현

---
+++ src/main/java/t221019/Stack01.java
@@ -0,0 +1,23 @@
+package t221019;
+
+public class Stack01 {
+
+    private int[] arr = new int[10000];
+    private int pointer = 0;
+
+    public Stack01() {
+    }
+
+    public Stack01(int size) {
+        this.arr = new int[size];
+    }
+
+    public void push(int value) {
+        this.arr[this.pointer] = value;
+        this.pointer++;
+    }
+
+    public int[] getArr() {
+        return this.arr;
+    }
+}
