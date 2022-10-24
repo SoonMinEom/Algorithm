64120b6df9282a147368373e09556c4038d3498e
soonmineom <skdlfma123@gmail.com>
2022-10-24 10:12:05 +0900

    PQ 이용한 풀이 추가

--- src/main/java/onclass/t221024/KthNum.java
+++ src/main/java/onclass/t221024/KthNum.java
@@ -1,8 +1,11 @@
 package onclass.t221024;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class KthNum {
+
+    int[] arr;
 
     public int[] solution ( int[] arr, int[][] commands) {
         int[] answer = new int[commands.length];
@@ -15,7 +18,34 @@ public class KthNum {
         return answer;
     }
 
+    public int getKthNum ( int[] command ) {
+        int frIdx = command[0];
+        int toIdx = command[1];
+        int nth = command[2];
+
+        int result = 0 ;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = frIdx-1 ; i <toIdx; i++) {
+            pq.add(arr[i]);
+        }
+
+        for (int i = 0 ; i < nth ; i ++) {
+            result = pq.poll();
+        }
+        return result;
+    }
+
+    public int[] solution2(int[] arr, int[][] commands) {
+        this.arr = arr;
+        int[] answer = new int[commands.length];
+        for (int i = 0; i < commands.length; i++) {
+            answer[i] = getKthNum(commands[i]);
+        }
+        return answer;
+    }
+
     public static void main(String[] args) {
+
         int[] arr = {1, 5, 2, 6, 3, 7, 4};
         int[][] commands = {
                 {2, 5, 3},
@@ -24,7 +54,7 @@ public class KthNum {
         };
 
         KthNum kthNum = new KthNum();
-        int[] result = kthNum.solution(arr,commands);
+        int[] result = kthNum.solution2(arr,commands);
 
         System.out.println(Arrays.toString(result));
 
