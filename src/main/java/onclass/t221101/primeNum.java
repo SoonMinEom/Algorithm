ee22cf113e323c30352e62a0376cd6e8fd10f0b5
soonmineom <skdlfma123@gmail.com>
2022-11-01 10:05:36 +0900

    템플릿 콜백... 적...용....?

--- src/main/java/onclass/t221101/primeNum.java
+++ src/main/java/onclass/t221101/primeNum.java
@@ -1,10 +1,11 @@
 package onclass.t221101;
 
 public class primeNum {
-    public boolean isPrime(int num) {
+
+    public boolean isPrime(int num, int type) {
         boolean result = true;
 
-        for (int i = 2; i < num; i++) {
+        for (int i = 2; i < range(num,type); i++) {
             if (num%i != 0) {
                 result = true;
             } else {
@@ -15,38 +16,51 @@ public class primeNum {
         return result;
     }
 
-    public boolean isPrime2(int num) {
-        boolean result = true;
-
-        for (int i = 2; i < (num/2) ; i++) {
-            if (num%i != 0) {
-                result = true;
-            } else {
-                result = false;
-                break;
-            }
+    public double range(int num, int type ) {
+        switch (type) {
+            case 1 :
+                return num;
+            case 2:
+                return num/2;
+            case 3:
+                return Math.sqrt(num)+1;
+            default:
+                return num;
         }
-        return result;
     }
 
-    public boolean isPrime3(int num) {
-        boolean result = true;
+//    public boolean isPrime2(int num) {
+//        boolean result = true;
+//
+//        for (int i = 2; i < (num/2) ; i++) {
+//            if (num%i != 0) {
+//                result = true;
+//            } else {
+//                result = false;
+//                break;
+//            }
+//        }
+//        return result;
+//    }
 
-        for (int i = 2; i <= Math.sqrt(num) ; i++) {
-            if (num%i != 0) {
-                result = true;
-            } else {
-                result = false;
-                break;
-            }
-        }
-        return result;
-    }
+//    public boolean isPrime3(int num) {
+//        boolean result = true;
+//
+//        for (int i = 2; i <= Math.sqrt(num) ; i++) {
+//            if (num%i != 0) {
+//                result = true;
+//            } else {
+//                result = false;
+//                break;
+//            }
+//        }
+//        return result;
+//    }
 
     public static void main(String[] args) {
         primeNum pn = new primeNum();
 
-        if(pn.isPrime3(91)) {
+        if(pn.isPrime(97,2) ){
             System.out.println("소수입니다.");
         } else {
             System.out.println("소수가 아닙니다.");
