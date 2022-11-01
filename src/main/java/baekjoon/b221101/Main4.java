eabb97d9f1fc96c014f0d956b6a8661b6766d2a3
soonmineom <skdlfma123@gmail.com>
2022-11-01 20:45:29 +0900

    강아지 그리기...

---
+++ src/main/java/baekjoon/b221101/Main4.java
@@ -0,0 +1,13 @@
+package baekjoon.b221101;
+
+public class Main4 {
+    public static void main(String[] args) {
+
+
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
