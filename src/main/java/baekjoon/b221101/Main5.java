f1ffff597e8f0d7e5c99bff4e7fdea104b7edb63
soonmineom <skdlfma123@gmail.com>
2022-11-01 20:45:39 +0900

    새싹 그리기...

---
+++ src/main/java/baekjoon/b221101/Main5.java
@@ -0,0 +1,14 @@
+package baekjoon.b221101;
+
+public class Main5 {
+    public static void main(String[] args) {
+
+
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
