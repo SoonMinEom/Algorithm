01398f05c8a61c6253d634d687e07988a0e357a4
soonmineom <skdlfma123@gmail.com>
2022-11-07 10:23:05 +0900

    PriorityQueue 활용

--- src/main/java/onclass/t221107/DividableNum.java
+++ src/main/java/onclass/t221107/DividableNum.java
@@ -3,6 +3,7 @@ package onclass.t221107;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class DividableNum {
     public int[] solution(int[] arr, int divisor) {
@@ -17,8 +18,10 @@ public class DividableNum {
         int[] answer = {};
 
         if(answerlist.size() ==0) {
-            answer = new int[1];
-            answer[0] = -1;
+//            answer = new int[1];
+//            answer[0] = -1;
+            // 한 줄로 표현
+            answer = new int[] {-1};
         } else {
             answer = new int[answerlist.size()];
             int idx = 0;
@@ -32,9 +35,33 @@ public class DividableNum {
         return answer;
     }
 
+    public int[] solution2(int[] arr, int divisor) {
+
+        // 자동으로 오름차순 정렬을 해줌.
+        PriorityQueue<Integer> answerlist = new PriorityQueue<>();
+
+        for (int i : arr) {
+            if(i%divisor == 0) {
+                answerlist.add(i);
+            }
+        }
+        int[] answer = {};
+
+        if(answerlist.size() ==0) {
+            answer = new int[] {-1};
+        } else {
+            answer = new int[answerlist.size()];
+            for (int i = 0; i < answer.length; i++) {
+                answer[i] = answerlist.poll();
+            }
+        }
+
+        return answer;
+    }
+
     public static void main(String[] args) {
         DividableNum dn = new DividableNum();
         int[] a = {5,9,7,10};
-        System.out.println(Arrays.toString(dn.solution(a,5)));
+        System.out.println(Arrays.toString(dn.solution2(a,5)));
     }
 }
