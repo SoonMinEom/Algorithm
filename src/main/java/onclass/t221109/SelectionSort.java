336efcf635314f179af4c42d40f60db120674ebd
soonmineom <skdlfma123@gmail.com>
2022-11-09 09:43:25 +0900

    선택정렬 과정 + 완성

---
+++ src/main/java/onclass/t221109/SelectionSort.java
@@ -0,0 +1,117 @@
+package onclass.t221109;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+
+        int[] arr = {2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
+
+        for (int i = 0; i < arr.length; i++) {
+            int minIdx = i;
+            for (int j = i+1 ; j < arr.length ; j++) {
+                if(arr[minIdx] > arr[j]) {
+                    minIdx = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[minIdx];
+            arr[minIdx] = temp;
+
+            System.out.println(Arrays.toString(arr));
+        }
+
+
+//        i = 1;
+//        minIdx = i;
+//        for (int j = i+1 ; j < arr.length ; j++) {
+//            if(arr[midIdx] > arr[j]) {
+//                minIdx = j;
+//            }
+//        }
+//
+//        temp = arr[i];
+//        arr[i] = arr[minIdx];
+//        arr[minIdx] = temp;
+//
+//        System.out.println(Arrays.toString(arr));
+//
+//        i = 2;
+//        minIdx = i;
+//        for (int j = i+1 ; j < arr.length ; j++) {
+//            if(arr[midIdx] > arr[j]) {
+//                minIdx = j;
+//            }
+//        }
+//
+//        temp = arr[i];
+//        arr[i] = arr[minIdx];
+//        arr[minIdx] = temp;
+//
+//        System.out.println(Arrays.toString(arr));
+//
+//        i = 3;
+//        minIdx = i;
+//        for (int j = i+1 ; j < arr.length ; j++) {
+//            if(arr[midIdx] > arr[j]) {
+//                minIdx = j;
+//            }
+//        }
+//
+//        temp = arr[i];
+//        arr[i] = arr[minIdx];
+//        arr[minIdx] = temp;
+//
+//        System.out.println(Arrays.toString(arr));
+//
+//        i = 4;
+//        minIdx = i;
+//        for (int j = i+1 ; j < arr.length ; j++) {
+//            ifarr[midIdx] > arr[j]) {
+//                minIdx = j;
+//            }
+//        }
+//
+//        temp = arr[i];
+//        arr[i] = arr[minIdx];
+//        arr[minIdx] = temp;
+//
+//        System.out.println(Arrays.toString(arr));
+//
+//        i = 5;
+//        minIdx = i;
+//        for (int j = i+1 ; j < arr.length ; j++) {
+//            if(arr[midIdx] > arr[j]) {
+//                minIdx = j;
+//            }
+//        }
+//
+//        temp = arr[i];
+//        arr[i] = arr[minIdx];
+//        arr[minIdx] = temp;
+//
+//        System.out.println(Arrays.toString(arr));
+//
+//        i = 6;
+//        minIdx = i;
+//        for (int j = i+1 ; j < arr.length ; j++) {
+//            if(arr[midIdx] > arr[j]) {
+//                minIdx = j;
+//            }
+//        }
+//
+//        temp = arr[i];
+//        arr[i] = arr[minIdx];
+//        arr[minIdx] = temp;
+//
+//        System.out.println(Arrays.toString(arr));
+
+
+
+
+
+
+
+    }
+}
