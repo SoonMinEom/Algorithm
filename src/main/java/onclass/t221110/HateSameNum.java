30a3f2a2875376dbb8601e1c1a166bf61cbd118b
soonmineom <skdlfma123@gmail.com>
2022-11-10 09:48:45 +0900

    같은 숫자 중복 제거 + 순서 유지, List 사용 + List의 마지막 값 확인 + Stack 사용 풀이

--- src/main/java/onclass/t221110/HateSameNum.java
+++ src/main/java/onclass/t221110/HateSameNum.java
@@ -14,9 +14,39 @@ public class HateSameNum {
            }
         }
 
-//        System.out.println(answerList);
+            // toArray() 메소드를 사용해서 List -> Array 변환.
+            // int[]는 원시타입 Array 이므로 추가작업 필요.
+//        int[] answer = answerList.stream().mapToInt(Integer::intValue).toArray();
 
-        int[] answer = answerList.stream().mapToInt(Integer::intValue).toArray();
+        int[] answer = new int[answerList.size()];
+
+        for (int i = 0; i < answerList.size(); i++) {
+            answer[i]=answerList.get(i);
+        }
+
+        System.out.println(Arrays.toString(answer));
+
+        return answer;
+    }
+
+    public int[] solution2(int []arr) {
+
+        Stack<Integer> answerStack = new Stack<>();
+
+        answerStack.push(arr[0]);
+        for (int i : arr) {
+            if(answerStack.peek() != i) {
+                answerStack.push(i);
+            }
+        }
+
+        int[] answer = new int[answerStack.size()];
+
+        for (int i = answerStack.size()-1 ; i >= 0 ; i--) {
+            answer[i] = answerStack.pop();
+        }
+
+        System.out.println(Arrays.toString(answer));
 
         return answer;
     }
@@ -26,7 +56,7 @@ public class HateSameNum {
         int[] arr = {1,1,3,3,0,1,1};
 
         HateSameNum hsn = new HateSameNum();
-        hsn.solution(arr);
+        hsn.solution2(arr);
     }
 
 }
