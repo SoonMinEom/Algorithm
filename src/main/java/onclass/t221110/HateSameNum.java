9168356918de27f63b092be880f1f8efad60dc63
soonmineom <skdlfma123@gmail.com>
2022-11-10 10:10:17 +0900

    같은 숫자 중복 제거 + 순서 유지, List 사용 + List의 마지막 값 확인 + Stack 사용 풀이 + 넣기 전에 비교

--- src/main/java/onclass/t221110/HateSameNum.java
+++ src/main/java/onclass/t221110/HateSameNum.java
@@ -3,7 +3,7 @@ package onclass.t221110;
 import java.util.*;
 //실패..
 public class HateSameNum {
-    public int[] solution(int []arr) {
+    public int[] solution(int[] arr) {
 
         List<Integer> answerList = new ArrayList<>();
 
@@ -18,8 +18,8 @@ public class HateSameNum {
             // int[]는 원시타입 Array 이므로 추가작업 필요.
 //        int[] answer = answerList.stream().mapToInt(Integer::intValue).toArray();
 
+        // 반복문 사용으로 List -> Array변환
         int[] answer = new int[answerList.size()];
-
         for (int i = 0; i < answerList.size(); i++) {
             answer[i]=answerList.get(i);
         }
@@ -29,7 +29,8 @@ public class HateSameNum {
         return answer;
     }
 
-    public int[] solution2(int []arr) {
+    // Stack 사용 풀이. 원리는 같음.
+    public int[] solution2(int[] arr) {
 
         Stack<Integer> answerStack = new Stack<>();
 
@@ -42,6 +43,7 @@ public class HateSameNum {
 
         int[] answer = new int[answerStack.size()];
 
+        // pop은 마지막 것 부터 나오므로, Index를 거꾸로
         for (int i = answerStack.size()-1 ; i >= 0 ; i--) {
             answer[i] = answerStack.pop();
         }
@@ -51,12 +53,28 @@ public class HateSameNum {
         return answer;
     }
 
+    public void solution3(int[] arr) {
+
+        List<Integer> answerList = new ArrayList<>();
+
+        answerList.add(arr[0]);
+        for (int i = 1; i < arr.length ; i++) {
+            if (arr[i] != arr[i-1]) {
+                answerList.add(arr[i]);
+            }
+        }
+
+        System.out.println(answerList);
+    }
+
     public static void main(String[] args) {
 
         int[] arr = {1,1,3,3,0,1,1};
 
         HateSameNum hsn = new HateSameNum();
+        hsn.solution(arr);
         hsn.solution2(arr);
+        hsn.solution3(arr);
     }
 
 }
