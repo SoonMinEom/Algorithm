d82723a3434b10fc4cf1b4fcff61b9fdac240931
soonmineom <skdlfma123@gmail.com>
2022-11-11 10:45:39 +0900

    비트 연산 활용 2진수, + 자릿수 맞추기 + replace로 # 바꾸기

--- src/main/java/onclass/t221111/SecretMap.java
+++ src/main/java/onclass/t221111/SecretMap.java
@@ -21,26 +21,46 @@ public class SecretMap {
 
     public String[] solution(int n, int[] arr1, int[] arr2) {
 
-        String[] sArr1 = new String[n];
-        String[] sArr2 = new String[n];
+//        String[] sArr1 = new String[n];
+//        String[] sArr2 = new String[n];
 
+//        for (int i = 0; i < n ; i++) {
+//                String binStr1 = Integer.toBinaryString(arr1[i]);
+//                sArr1[i] = "0".repeat(n - binStr1.length()) + binStr1;
+//
+//                String binStr2 = Integer.toBinaryString(arr2[i]);
+//                sArr2[i] = "0".repeat(n - binStr2.length()) + binStr2;
+//        }
+
+//        for (int i = 0; i < n ; i++) {
+//            String binStr1 = Integer.toBinaryString(arr1[i]);
+//            for (int j = 0; j < n- binStr1.length(); j++) {
+//                binStr1 = 0 + binStr1;
+//            }
+//            sArr1[i] = binStr1;
+//
+//            String binStr2 = Integer.toBinaryString(arr2[i]);
+//            for (int j = 0; j < n- binStr2.length(); j++) {
+//                binStr2 = 0 + binStr2;
+//            }
+//            sArr2[i] = binStr2;
+//        }
+//
+//        System.out.println(Arrays.toString(sArr1));
+//        System.out.println(Arrays.toString(sArr2));
+
+        String[] answer = new String[n];
         for (int i = 0; i < n ; i++) {
-            //arr1
-            for (int j = 0; j < n ; j++) {
-
-                String binStr1 = Integer.toBinaryString(arr1[i]);
-                sArr1[i] = "0".repeat(n - binStr1.length()) + binStr1;
-
-                String binStr2 = Integer.toBinaryString(arr2[i]);
-                sArr2[i] = "0".repeat(n - binStr2.length()) + binStr2;
-            }
+            answer[i] =Integer.toBinaryString(arr1[i] | arr2[i]);
         }
 
-        System.out.println(Arrays.toString(sArr1));
-        System.out.println(Arrays.toString(sArr2));
+        for (int i = 0; i < n ; i++) {
+            answer[i] = answer[i].replace("1","#").replace("0"," ");
+            answer[i] = " ".repeat(n-answer[i].length()) + answer[i];
+        }
 
+//        System.out.println(Arrays.toString(answer));
 
-        String[] answer = {};
         return answer;
 
     }
@@ -50,7 +70,8 @@ public class SecretMap {
         int[] arr1 = {9, 20, 28, 18, 11};
         int[] arr2 = {30, 1, 21, 17, 28};
         SecretMap sm = new SecretMap();
-//        System.out.println(sm.toBinary(30));
         sm.solution(n, arr1, arr2);
+
+
     }
 }
