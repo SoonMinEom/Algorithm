dd1108b5bba2391cae994b05c47934a6436e3462
soonmineom <skdlfma123@gmail.com>
2022-11-11 09:33:10 +0900

    2진수 리턴 연습

---
+++ src/main/java/onclass/t221111/SecretMap.java
@@ -0,0 +1,48 @@
+package onclass.t221111;
+
+public class SecretMap {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+
+//        String[] strArr1 = new String[n];
+//        for (int i : arr1) {
+//            Integer.toBinaryString(i);
+//        }
+//
+//        String[] strArr2 = new String[n];
+//        for (int i : arr2) {
+//            Integer.toBinaryString(i);
+//        }
+//
+//        for (int i = 0; i < ; i++) {
+//
+//        }
+//
+//        if(strArr1[1].charAt(0) != strArr2[1].charAt(0)) {
+//            strArr1[1].charAt(0) = '0';
+//            strArr2[1] = "0";
+//        }
+
+//        StringBuilder result = new StringBuilder();
+//        while (arr1[1] > 0) {
+//            result.append(arr1[1] % 2);
+//            arr1[1] = arr1[1] /2;
+//        }
+//        arr1[1] / 2;
+//        arr1[1] % 2;
+//
+        String[] answer = {};
+        return answer;
+
+    }
+
+    public static void main(String[] args) {
+
+        StringBuilder result = new StringBuilder();
+        int num = 9;
+        while (num > 0) {
+            result.append(num % 2);
+            num = num /2;
+        }
+        System.out.println(result);
+    }
+}
