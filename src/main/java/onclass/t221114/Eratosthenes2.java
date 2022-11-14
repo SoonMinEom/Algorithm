d012ac001dc1051b587e5a0ea86247a20c14389b
soonmineom <skdlfma123@gmail.com>
2022-11-14 10:11:17 +0900

    for 문으로 반복 처리, 소수 개수 출력

--- src/main/java/onclass/t221114/Eratosthenes2.java
+++ src/main/java/onclass/t221114/Eratosthenes2.java
@@ -15,11 +15,52 @@ public class Eratosthenes2 {
         }
         System.out.println(Arrays.toString(arr));
 
-        // 2를 제외한 2의 배수 자리에 0 넣기
-        for (int i = 2; i < arr.length ; i+=2) {
-            arr[i] = 0;
+//        // 2를 제외한 2의 배수 자리에 0 넣기
+//        for (int i = 2; i < arr.length ; i+=2) {
+//            arr[i] = 0;
+//        }
+//        System.out.println(Arrays.toString(arr));
+//
+//        // 3를 제외한 3의 배수 자리에 0 넣기
+//        for (int i = 4; i < arr.length ; i+=3) {
+//            arr[i] = 0;
+//        }
+//        System.out.println(Arrays.toString(arr));
+//
+//        // 4를 제외한 4의 배수 자리에 0 넣기
+//        for (int i = 6; i < arr.length ; i+=4) {
+//            arr[i] = 0;
+//        }
+//        System.out.println(Arrays.toString(arr));
+//
+//        // 5를 제외한 5의 배수 자리에 0 넣기
+//        for (int i = 8; i < arr.length ; i+=5) {
+//            arr[i] = 0;
+//        }
+//        System.out.println(Arrays.toString(arr));
+//
+//        // 7를 제외한 7의 배수 자리에 0 넣기
+//        for (int i = 12; i < arr.length ; i+=7) {
+//            arr[i] = 0;
+//        }
+//        System.out.println(Arrays.toString(arr));
+
+        // 위의 식을 for문으로 반복 처리
+        for (int i = 2; i*i <= length ; i++) {
+            for (int j = 2+(i-2)*2; j < arr.length ; j +=i) {
+                arr[j] = 0;
+            }
         }
         System.out.println(Arrays.toString(arr));
+
+        // 소수의 개수 출력
+        int cnt = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
 
 
 
