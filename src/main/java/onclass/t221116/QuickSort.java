042cdd74b7f0c68429d7974add94ee1e1516fa93
soonmineom <skdlfma123@gmail.com>
2022-11-16 09:33:29 +0900

    퀵 정렬 Array 구현 중..

---
+++ src/main/java/onclass/t221116/QuickSort.java
@@ -0,0 +1,26 @@
+package onclass.t221116;
+
+public class QuickSort {
+    public void sort(int[] arr) {
+
+        int pivotIdx = arr.length/2;
+        int leftIdx = 0;
+        int rightIdx = arr.length-1;
+
+        while () {
+
+        }
+
+
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {20, 18, 5, 19, 40, 50, 5, 25};
+
+        QuickSort qs = new QuickSort();
+        qs.sort(arr);
+
+    }
+}
