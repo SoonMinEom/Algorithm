546d3da26ec3521255ad91bd0b0adc4aeb5b9de1
soonmineom <skdlfma123@gmail.com>
2022-11-22 09:23:01 +0900

    Print1To100 - 탈출조건, 1씩 커지는 로직

--- src/main/java/onclass/t221122/Print1To100.java
+++ src/main/java/onclass/t221122/Print1To100.java
@@ -2,7 +2,18 @@ package onclass.t221122;
 
 public class Print1To100 {
     public static void print1To100(int n) {
+        // 탈출 조건
+        if(n>100) {
+            return;
+        }
+
+        // 원하는 로직
         System.out.println(n);
+
+        // 탈출 조건을 만족시키기 위한 로직
+        n++;
+
+        // 재귀
         print1To100(n);
     }
     public static void main(String[] args) {
