e54fcb2028c0bff1e92891bf1730f357baaba6db
soonmineom <skdlfma123@gmail.com>
2022-11-25 09:48:34 +0900

    최소공배수 구하기, 재귀 없이

--- src/main/java/onclass2/t221125/GreatestCommonDivisor.java
+++ src/main/java/onclass2/t221125/GreatestCommonDivisor.java
@@ -9,8 +9,16 @@ public class GreatestCommonDivisor {
         else return gcd(a, b-a);
     }
 
+    public static int gcd2(int a, int b) {
+        while (a !=b) {
+            if (a > b) a -= b;
+            else b -= a;
+        }
+        return a;
+    }
+
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
-        System.out.println(gcd(sc.nextInt(),sc.nextInt()));
+        System.out.println(gcd2(112,42));
     }
 }
