64d3f3e60fa8e090bdec99e80cb5b50cc7b38c6d
soonmineom <skdlfma123@gmail.com>
2022-11-25 09:17:47 +0900

    최소공배수 구하기, 두 수가 같아질 때까지 계속해서 빼는 방법

---
+++ src/main/java/onclass2/t221125/GreatestCommonDivisor.java
@@ -0,0 +1,13 @@
+package onclass2.t221125;
+
+public class GreatestCommonDivisor {
+    public static int gcd(int a, int b) {
+        if(a == b) return a;
+        else if(a > b) return gcd(a-b, b);
+        else return gcd(a, b-a);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(gcd(196,42));
+    }
+}
