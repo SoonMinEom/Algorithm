ba787fbfeacebb5e6917e9e87e29f0059350a40f
soonmineom <skdlfma123@gmail.com>
2022-11-28 09:10:34 +0900

    모든 알파벳 조합, 실습1

---
+++ src/main/java/onclass2/t221128/AllAlphabet.java
@@ -0,0 +1,16 @@
+package onclass2.t221128;
+
+public class AllAlphabet {
+    public void solution() {
+        char alpha = 65;
+        while (alpha <91){
+            System.out.println(alpha++);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        AllAlphabet aa = new AllAlphabet();
+        aa.solution();
+    }
+}
