8e43759d30004900de34f05b0b1e4125eae2b62f
soonmineom <skdlfma123@gmail.com>
2022-11-28 09:18:52 +0900

    모든 알파벳 조합, 실습2

--- src/main/java/onclass2/t221128/AllAlphabet.java
+++ src/main/java/onclass2/t221128/AllAlphabet.java
@@ -1,12 +1,25 @@
 package onclass2.t221128;
 
+import java.util.Arrays;
+
 public class AllAlphabet {
     public void solution() {
-        char alpha = 65;
-        while (alpha <91){
-            System.out.println(alpha++);
+        char[] alpha = new char[26];
+        char[] beta =  new char[26];
+
+        char start = 65;
+        for (int i = 0; i < alpha.length; i++) {
+            alpha[i] = start++;
+            beta[i] = alpha[i];
         }
 
+        String answer = "";
+        for (int i = 0; i < alpha.length; i++) {
+            for (int j = 0; j < beta.length; j++) {
+                answer=(alpha[i]+"")+(beta[j]+"");
+                System.out.println(answer);
+            }
+        }
     }
 
     public static void main(String[] args) {
