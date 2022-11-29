2ec1dabb808726104d6cb819b9df08c891d1be64
soonmineom <skdlfma123@gmail.com>
2022-11-29 11:06:42 +0900

    RadixSort 완성

--- src/main/java/onclass2/t221129/RadixSort.java
+++ src/main/java/onclass2/t221129/RadixSort.java
@@ -1,8 +1,6 @@
 package onclass2.t221129;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class RadixSort {
     // 한 자리수 정렬하기
@@ -27,10 +25,39 @@ public class RadixSort {
         return digit;
     }
 
+    //큐배열에 담기
+    public static int[] queueArr(int[] arr, int digit) {
+        // 10칸짜리 큐 배열 만들기
+        Queue<Integer>[] qa = new Queue[10];
+        for (int i = 0; i < qa.length ; i++) {
+            qa[i] = new ArrayDeque<>();
+        }
+
+        // 자릿수에 맞는 큐에 넣기.
+        for (int i = 0; i < arr.length; i++) {
+            int divisor = (int)Math.pow(10,digit-1);
+            qa[Math.floorDiv(arr[i],divisor)%10].add(arr[i]);
+        }
+
+        //다시 배열로 돌리기
+        int idx = 0;
+        for (int i = 0; i < qa.length; i++) {
+            while (!qa[i].isEmpty()) {
+                arr[idx++] =qa[i].poll();
+            }
+        }
+        return arr;
+    }
+
     public static void main(String[] args) {
-        int[] arr = {7,4,5,9,1,0,23,520};
-        RadixSort rs = new RadixSort();
-//        rs.sort(arr);
-        System.out.println(Arrays.toString(rs.maxDigit(arr)));
+        int[] arr = {7, 4, 5, 9, 1, 0, 20};
+        int[] digits = maxDigit(arr);
+        System.out.println(Arrays.toString(maxDigit(arr)));
+
+        for (int digit : digits) {
+            queueArr(arr,digit);
+        }
+
+        System.out.println(Arrays.toString(arr));
     }
 }
