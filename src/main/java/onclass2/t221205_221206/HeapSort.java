c8655bda4b443dc45cfbb890e12f8bfdf9e9743f
soonmineom <skdlfma123@gmail.com>
2022-12-06 10:27:59 +0900

    힙된 것을 다시 힙적용 반복 시켜 정렬 완성

--- src/main/java/onclass2/t221205_221206/HeapSort.java
+++ src/main/java/onclass2/t221205_221206/HeapSort.java
@@ -3,22 +3,18 @@ package onclass2.t221205_221206;
 import java.util.Arrays;
 
 public class HeapSort {
-    public static void childIdx(int num) {
-        System.out.printf("leftIdx : %d\n", 2*num+1);
-        System.out.printf("rightIdx : %d", 2*num+2);
-    }
-    public static int[] sort(int[] arr, int parentIdx){
+    public static int[] makeHeap(int[] arr, int parentIdx, int arrSize){
         int leftIdx = 2 * parentIdx + 1;
         int rightIdx = 2 * parentIdx + 2;
         int greaterIdx = parentIdx;
 
         // 왼쪽 자식이 부모보다 큰 경우
-        if(leftIdx < arr.length && arr[leftIdx] > arr[greaterIdx] ){
+        if(leftIdx < arrSize && arr[leftIdx] > arr[greaterIdx] ){
             greaterIdx = leftIdx;
         }
 
         // 오른쪽 자식이 greater 보다 크면
-        if(rightIdx < arr.length && arr[rightIdx] > arr[greaterIdx]) {
+        if(rightIdx < arrSize && arr[rightIdx] > arr[greaterIdx]) {
             greaterIdx = rightIdx;
         }
 
@@ -27,7 +23,7 @@ public class HeapSort {
             int temp = arr[parentIdx];
             arr[parentIdx] = arr[greaterIdx];
             arr[greaterIdx] = temp;
-            sort(arr, greaterIdx);
+            makeHeap(arr, greaterIdx, arrSize);
         }
 
 
@@ -37,9 +33,44 @@ public class HeapSort {
     public static void main(String[] args) {
         int[] arr = {10,9,5,8,3,2,4,6,7,1};
 
-        for (int i = (arr.length-2); i >0 ; i--) {
-            System.out.println(Arrays.toString(sort(arr,i)));
+        for (int j = 0 ; j < arr.length ; j++) {
+
+            for (int i = (arr.length-2) / 2; i >=0 ; i--) {
+                System.out.println(Arrays.toString(makeHeap(arr,i, arr.length-j)));
+            }
+
+            int temp = arr[0];
+            arr[0] = arr[arr.length-(j+1)];
+            arr[arr.length-(j+1)] = temp;
+            System.out.println(Arrays.toString(arr));
         }
+//
+//        for (int i = (arr.length-2) / 2; i >=0 ; i--) {
+//            System.out.println(Arrays.toString(makeHeap(arr,i, arr.length)));
+//        }
+//
+//        int temp = arr[0];
+//        arr[0] = arr[arr.length-1];
+//        arr[arr.length-1] = temp;
+//        System.out.println(Arrays.toString(arr));
+//
+//        for (int i = (arr.length-2) / 2; i >=0 ; i--) {
+//            System.out.println(Arrays.toString(makeHeap(arr,i, arr.length-1)));
+//        }
+//
+//        temp = arr[0];
+//        arr[0] = arr[arr.length-2];
+//        arr[arr.length-2] = temp;
+//        System.out.println(Arrays.toString(arr));
+//
+//        for (int i = (arr.length-2) / 2; i >=0 ; i--) {
+//            System.out.println(Arrays.toString(makeHeap(arr,i, arr.length-2)));
+//        }
+
+
+
+
+
 
     }
 }
