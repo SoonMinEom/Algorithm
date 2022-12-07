9bc064334ad1881d0ee4612412e2c369975e3ae9
soonmineom <skdlfma123@gmail.com>
2022-12-07 10:26:13 +0900

    종이자르기

---
+++ src/main/java/programers/level_0/종이자르기.java
@@ -0,0 +1,10 @@
+package programers.level_0;
+//https://school.programmers.co.kr/learn/courses/30/lessons/120922
+
+public class 종이자르기 {
+    public int solution(int M, int N) {
+        // int answer = (N-1) + N*(M-1);
+        int answer = M*N -1;
+        return answer;
+    }
+}
