cb089f7c77b59267793354828e95236df9d91340
soonmineom <skdlfma123@gmail.com>
2022-12-08 10:50:14 +0900

    7의 개수는 문자열로 바꾸지 않고도 구할 수 있다!

---
+++ src/main/java/programers/level_0/칠의개수.java
@@ -0,0 +1,36 @@
+package programers.level_0;
+
+import java.util.Arrays;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/120912
+public class 칠의개수 {
+    public static int solution(int[] array) {
+        int cnt = 0;
+
+        for (int i = 0; i < array.length ; i++) {
+            String check = String.valueOf(array[i]);
+            for (int j = 0; j < check.length(); j++) {
+                if (check.charAt(j) == '7') cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    public static int solution2(int[] array) {
+        int cnt = 0;
+
+        for (int a : array) {
+            while (a > 0) {
+                if (a % 10 ==7) cnt++;
+                a /= 10;
+            }
+        }
+        return cnt;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {7, 77, 17};
+        System.out.println(solution(arr));
+    }
+}
