6865c1b1bf9e798f05c90cb4fb5342774f019c9d
soonmineom <skdlfma123@gmail.com>
2022-12-09 09:45:41 +0900

    DynamicLCS 값 채우기

--- src/main/java/onclass2/t221208_221209/DynamicLCS.java
+++ src/main/java/onclass2/t221208_221209/DynamicLCS.java
@@ -1,16 +1,27 @@
 package onclass2.t221208_221209;
 
 public class DynamicLCS {
+    public static int max(int a, int b) {
+        if (a>b) return a;
+        else return b;
+    }
     public static void main(String[] args) {
         String str1 = "ABCDCBA"; // 열
         String str2 = "DCABDC"; // 행
 
-        int[][] arr = new int[str2.length()][str1.length()];
+        int[][] dp = new int[str2.length()+1][str1.length()+1];
 
-        for (int i = 0; i < str2.length(); i++) {
-            for (int j = 0; j < str1.length(); j++) {
+        for (int i = 1; i <= str2.length(); i++) {
+            for (int j = 1; j <= str1.length(); j++) {
                 System.out.printf("%s %s\n",str2.charAt(i), str1.charAt(j));
+                // 두 개가 같으면 대각선에서 가지고오기
+                if (dp[i-1][j] == dp[i][j-1]) dp[i][j] = dp[i-1][j-1];
+                // 두 개가 다르면 왼쪽, 위쪽 중 큰 값을 가져오기
+                else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
             }
         }
+
+
+
     }
 }
