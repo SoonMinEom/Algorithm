edcdf423dcc7e52533553facd725933c3cf01ff9
soonmineom <skdlfma123@gmail.com>
2022-12-12 09:31:30 +0900

    DynamicMCP, 2 : (0,0)에 값 채워 넣기

--- src/main/java/onclass2/t221212/DynamicMCP.java
+++ src/main/java/onclass2/t221212/DynamicMCP.java
@@ -4,11 +4,19 @@ import java.util.Arrays;
 
 public class DynamicMCP {
     public static int mcp(int[][] arr) {
+        // 0 으로 초기화 된 3x3 배열 선언
         int[][] dp = new int[arr.length][arr[0].length];
 
-//        for (int[] ints : dp) {
-//            System.out.println(Arrays.toString(ints));
-//        }
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                if ( i == 0 && j == 0) dp[i][j] = arr[i][j];
+            }
+        }
+
+
+        for (int[] ints : dp) {
+            System.out.println(Arrays.toString(ints));
+        }
 
         return 0;
     }
