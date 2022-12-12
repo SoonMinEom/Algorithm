505da9bcead187b1ea45f551f35f690fcbcefa88
soonmineom <skdlfma123@gmail.com>
2022-12-12 10:04:23 +0900

    DynamicMCP, 4 + 5 : 첫 열 채우기 + 나머지 채우기

--- src/main/java/onclass2/t221212/DynamicMCP.java
+++ src/main/java/onclass2/t221212/DynamicMCP.java
@@ -3,6 +3,10 @@ package onclass2.t221212;
 import java.util.Arrays;
 
 public class DynamicMCP {
+
+    public static int min(int a, int b, int c) {
+        return (a < b) ? ((a < c) ? a : c) : ((b < c) ? b : c);
+    }
     public static int mcp(int[][] arr) {
         // 0 으로 초기화 된 3x3 배열 선언
         int[][] dp = new int[arr.length][arr[0].length];
@@ -13,6 +17,16 @@ public class DynamicMCP {
                 if ( i == 0 && j == 0) dp[i][j] = arr[i][j];
                 //첫 행 나머지 채우기
                 else if ( i == 0 && j != 0 ) dp[i][j] = dp[i][j-1] + arr[i][j];
+                // 첫 열 나머지 채우기
+                else if ( i != 0 && j == 0 ) dp[i][j] = dp[i-1][j] + arr[i][j];
+                // 나머지 부분 채우기.
+                else {
+                    int a = dp[i-1][j] + arr[i][j];
+                    int b = dp[i][j-1] + arr[i][j];
+                    int c = dp[i-1][j-1] + arr[i][j];
+                    dp[i][j] = min(a,b,c);
+                }
+
             }
         }
 
