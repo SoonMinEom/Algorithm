c1fb3c9baf2b55d1b4deb60b489aa9f29dde1fcf
soonmineom <skdlfma123@gmail.com>
2022-12-12 09:21:35 +0900

    DynamicMCP, 1 : 3x3 배열만큼 dp 선언하기

---
+++ src/main/java/onclass2/t221212/DynamicMCP.java
@@ -0,0 +1,20 @@
+package onclass2.t221212;
+
+import java.util.Arrays;
+
+public class DynamicMCP {
+    public static int mcp(int[][] arr) {
+        int[][] dp = new int[arr.length][arr[0].length];
+
+//        for (int[] ints : dp) {
+//            System.out.println(Arrays.toString(ints));
+//        }
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = {{1,3,2},{4,6,2},{1,2,4}};
+        mcp(arr);
+    }
+}
