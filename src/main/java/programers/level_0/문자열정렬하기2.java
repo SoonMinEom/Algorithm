5d70dd6f07f08340127993cfc86af942eceddf4d
soonmineom <skdlfma123@gmail.com>
2023-01-03 17:53:42 +0900

    문자열 정렬하기. toCharArray(), String a = new String(charArray)

---
+++ src/main/java/programers/level_0/문자열정렬하기2.java
@@ -0,0 +1,29 @@
+package programers.level_0;
+
+import java.util.Arrays;
+
+public class 문자열정렬하기2 {
+    public String solution(String my_string) {
+        // 소문자로
+        my_string = my_string.toLowerCase();
+
+        // char 배열로 만들기
+        char[] chars = my_string.toCharArray();
+//        char[] chars = new char[my_string.length()];
+//        for (int i = 0; i <my_string.length() ; i++) {
+//            chars[i] = my_string.charAt(i);
+//        }
+
+        // 정렬하기
+        Arrays.sort(chars);
+
+        // String 으로 합치기
+        String answer = new String(chars);
+//        String answer = "";
+//        for (char aChar : chars) {
+//            answer += aChar;
+//        }
+
+        return answer;
+    }
+}
