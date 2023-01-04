c23ebde66dc71ba4109c68d8478f0e41760dfa44
soonmineom <skdlfma123@gmail.com>
2023-01-04 17:42:04 +0900

    세균증식. 제곱은 Math.pow() 활용도 좋아

---
+++ src/main/java/programers/level_0/세균증식.java
@@ -0,0 +1,9 @@
+package programers.level_0;
+// https://school.programmers.co.kr/learn/courses/30/lessons/120910
+
+public class 세균증식 {
+    public int solution(int n, int t) {
+        int answer = n * (int) Math.pow(2, t);
+        return answer;
+    }
+}
