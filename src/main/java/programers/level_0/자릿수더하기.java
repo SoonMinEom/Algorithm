fc23764d8136e77cc1714f6fbc3827c1c9064765
soonmineom <skdlfma123@gmail.com>
2023-01-05 16:53:24 +0900

    자릿수 더하기, 10으로 나머지 연산과 나누기를 사용

---
+++ src/main/java/programers/level_0/자릿수더하기.java
@@ -0,0 +1,13 @@
+package programers.level_0;
+// https://school.programmers.co.kr/learn/courses/30/lessons/120906
+
+public class 자릿수더하기 {
+    public int solution(int n) {
+        int answer = 0;
+        while (n > 0) {
+            answer += n%10;
+            n /= 10;
+        }
+        return answer;
+    }
+}
