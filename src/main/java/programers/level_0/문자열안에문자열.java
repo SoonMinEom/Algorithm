6b0de1adf6b613ef2612e37d368a87c7f2fc3402
soonmineom <skdlfma123@gmail.com>
2023-01-05 16:35:43 +0900

    문자열안에 문자열 찾기는 .contains() 메소드가 있다!

---
+++ src/main/java/programers/level_0/문자열안에문자열.java
@@ -0,0 +1,9 @@
+package programers.level_0;
+// https://school.programmers.co.kr/learn/courses/30/lessons/120908
+
+public class 문자열안에문자열 {
+    public int solution(String str1, String str2) {
+        if (str1.contains(str2)) return 1;
+        else return 2;
+    }
+}
