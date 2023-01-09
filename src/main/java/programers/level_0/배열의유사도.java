35341bca5ee0b538dd28e6ae13df6c791392c27a
soonmineom <skdlfma123@gmail.com>
2023-01-09 14:00:05 +0900

    배열의 유사도. for문 두 번 돌리는 전수조사 해버리기

---
+++ src/main/java/programers/level_0/배열의유사도.java
@@ -0,0 +1,14 @@
+package programers.level_0;
+
+public class 배열의유사도 {
+    public int solution(String[] s1, String[] s2) {
+        int answer = 0;
+        for (String s : s1) {
+            for (String ss : s2) {
+                if (s.equals(ss)) answer++;
+            }
+        }
+        return answer;
+    }
+
+}
