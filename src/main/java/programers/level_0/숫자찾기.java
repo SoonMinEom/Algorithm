7ef0a34b7f95eeafa4a4b70988e9f6d67d989fd6
soonmineom <skdlfma123@gmail.com>
2023-01-09 13:26:36 +0900

    String.valueOf() .contains() .indexOf() 를 기억하자 indexOf() 는 매게변수도 문자타입으로 해줘야 잘 찾아짐

---
+++ src/main/java/programers/level_0/숫자찾기.java
@@ -0,0 +1,15 @@
+package programers.level_0;
+// https://school.programmers.co.kr/learn/courses/30/lessons/120904
+
+public class 숫자찾기 {
+    public int solution(int num, int k) {
+        int answer = 0;
+        String number = String.valueOf(num);
+        if (number.contains(String.valueOf(k))) {
+            answer = number.indexOf(String.valueOf(k))+1;
+        } else {
+            answer = -1;
+        }
+        return answer;
+    }
+}
